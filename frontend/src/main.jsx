d08e6ad3d3e0e0ead3435ebdc50925b3d14c9f6b
Faruk Spahic <farukspahictz@gmail.com>
2024-05-05 15:44:48 +0200

    Created separate frontend and backend folder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./i18n";
+import "./i18n.js";
 import { ThemeProvider } from "./hooks/useTheme.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
