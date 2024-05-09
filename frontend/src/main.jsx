4346d27c013cb9cdee9856991506a4d4d16fb9b9
Omar Corbic <omar_corbic@hotmail.com>
2024-05-09 11:07:43 +0200

    Wrapped the application in suspense component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -6,12 +6,15 @@ import "./i18n.js";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { ThemeProvider } from "./hooks/useTheme.jsx";
+import Loader from "./components/Loader.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-      <ToastContainer />
-    </ThemeProvider>
+    <Suspense fallback={<Loader />}>
+      <ThemeProvider>
+        <App />
+        <ToastContainer />
+      </ThemeProvider>
+    </Suspense>
   </React.StrictMode>
 );
